0b41fdcdc1f117710ff3d722936d9cb0b75d774e
unknown <arnobm97@gmail.com>
2025-06-17 13:27:45 +0600

    projects cards height increased

--- portfolio-website/components/projects.jsx
+++ portfolio-website/components/projects.jsx
@@ -88,7 +88,7 @@ export default function Projects() {
               }`}
               style={{ transitionDelay: `${index * 200}ms` }}
             >
-              <div className="relative h-48 overflow-hidden">
+              <div className="relative h-56 overflow-hidden">
                 <img
                   src={project.image || "/placeholder.svg"}
                   alt={project.title}
